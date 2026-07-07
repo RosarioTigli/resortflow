41c3247c4f4f1a80c13232a2eebdf8ac04d74bd9
Rosario Tigli <rosariobattistaa@gmail.com>
2026-07-07 21:33:15 +0200

    Prima versione ResortFlow

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="card">
+
+        <h1>🏖️ Villa dei Tigli</h1>
+        <p className="subtitle">Resort & SPA</p>
+
+        <div className="ombrellone">
+          Ombrellone 18
+        </div>
+
+        <button>🍽️ Ordina Pranzo</button>
+
+        <button>🍹 Drink & Bar</button>
+
+        <button>🍨 Gelati e Dessert</button>
+
+        <button>🛎️ Chiama il Personale</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
