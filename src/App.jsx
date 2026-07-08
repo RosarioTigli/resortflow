54c87701e6fb9c9d47078ba98a708c614c6c8949
Rosario Tigli <rosariobattistaa@gmail.com>
2026-07-08 21:01:19 +0200

    Sprint 7 - QR Code dinamico e Welcome Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MenuPage from "./pages/MenuPage";
 import KitchenPage from "./pages/KitchenPage";
@@ -9,6 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/18" replace />} />
         <Route path="/:umbrellaId" element={<HomePage />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/kitchen" element={<KitchenPage />} />
